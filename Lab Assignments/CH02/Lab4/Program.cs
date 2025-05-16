8fdcd0da0e18b0a553d8ba35f11e369b006d24ef
Cami-Carstens <camisuec@aol.com>
2025-05-16 17:04:47 -0500

    Lab1-initial-commit

---
+++ Lab Assignments/CH02/Lab4/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+const double KM_PER_MILE = 1.6;
+
+Console.Write("Please enter distance in miles: ");
+
+string input = Console.ReadLine();
+
+double miles = Convert.ToDouble(input);
+
+double kilometers = miles * KM_PER_MILE;
+
+Console.WriteLine("There are {0} miles in {1} kilometers", miles, kilometers);
