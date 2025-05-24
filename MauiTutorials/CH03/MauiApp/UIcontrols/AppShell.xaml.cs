b07560260435a92226a4ea209cdd924192e5576c
Cami-Carstens <camisuec@aol.com>
2025-05-24 13:46:10 -0500

    GradeCalcMauiApp-CH03

---
+++ MauiTutorials/CH03/MauiApp/UIcontrols/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace UIcontrols
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MauiTutorials/CH03/MauiApp/UIcontrols/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace UIcontrols
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
