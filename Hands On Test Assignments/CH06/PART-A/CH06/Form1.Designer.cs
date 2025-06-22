65c7e6d991258664a940706cb0def2de24bc7694
Cami-Carstens <camisuec@aol.com>
2025-06-22 00:32:29 -0500

    CH06-HOT

---
+++ Hands On Test Assignments/CH06/PART-A/CH06/Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace CH06
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtAuthor = new System.Windows.Forms.TextBox();
+            this.txtIsbn = new System.Windows.Forms.TextBox();
+            this.txtKeyword = new System.Windows.Forms.TextBox();
+            this.btnAuthor = new System.Windows.Forms.Button();
+            this.btnIsbn = new System.Windows.Forms.Button();
+            this.btnKeyword = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lblResultName = new System.Windows.Forms.Label();
+            this.lblResultAuthor = new System.Windows.Forms.Label();
+            this.lblResultDescript = new System.Windows.Forms.Label();
+            this.lblResultIsbn = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.LightGray;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(-7, 2);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(962, 49);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "PAST PERFECT BOOKS AND ANTIQUES\r\n";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(99, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Author:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(100, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "ISBN:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(99, 189);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Keyword:";
+            // 
+            // txtAuthor
+            // 
+            this.txtAuthor.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAuthor.Location = new System.Drawing.Point(225, 85);
+            this.txtAuthor.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtAuthor.Name = "txtAuthor";
+            this.txtAuthor.Size = new System.Drawing.Size(298, 27);
+            this.txtAuthor.TabIndex = 4;
+            // 
+            // txtIsbn
+            // 
+            this.txtIsbn.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtIsbn.Location = new System.Drawing.Point(225, 133);
+            this.txtIsbn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtIsbn.Name = "txtIsbn";
+            this.txtIsbn.Size = new System.Drawing.Size(298, 27);
+            this.txtIsbn.TabIndex = 5;
+            // 
+            // txtKeyword
+            // 
+            this.txtKeyword.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtKeyword.Location = new System.Drawing.Point(225, 189);
+            this.txtKeyword.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtKeyword.Name = "txtKeyword";
+            this.txtKeyword.Size = new System.Drawing.Size(298, 27);
+            this.txtKeyword.TabIndex = 6;
+            // 
+            // btnAuthor
+            // 
+            this.btnAuthor.BackColor = System.Drawing.Color.AliceBlue;
+            this.btnAuthor.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAuthor.Location = new System.Drawing.Point(580, 83);
+            this.btnAuthor.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnAuthor.Name = "btnAuthor";
+            this.btnAuthor.Size = new System.Drawing.Size(113, 28);
+            this.btnAuthor.TabIndex = 7;
+            this.btnAuthor.Text = "Search";
+            this.btnAuthor.UseVisualStyleBackColor = false;
+            this.btnAuthor.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnIsbn
+            // 
+            this.btnIsbn.BackColor = System.Drawing.Color.AliceBlue;
+            this.btnIsbn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIsbn.Location = new System.Drawing.Point(580, 130);
+            this.btnIsbn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnIsbn.Name = "btnIsbn";
+            this.btnIsbn.Size = new System.Drawing.Size(113, 30);
+            this.btnIsbn.TabIndex = 8;
+            this.btnIsbn.Text = "Search";
+            this.btnIsbn.UseVisualStyleBackColor = false;
+            this.btnIsbn.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnKeyword
+            // 
+            this.btnKeyword.BackColor = System.Drawing.Color.AliceBlue;
+            this.btnKeyword.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnKeyword.Location = new System.Drawing.Point(580, 189);
+            this.btnKeyword.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnKeyword.Name = "btnKeyword";
+            this.btnKeyword.Size = new System.Drawing.Size(113, 28);
+            this.btnKeyword.TabIndex = 9;
+            this.btnKeyword.Text = "Search";
+            this.btnKeyword.UseVisualStyleBackColor = false;
+            this.btnKeyword.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(24, 240);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(117, 39);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Name:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(24, 410);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(197, 39);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Description:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(24, 328);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(126, 39);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Author:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(24, 500);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(107, 39);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "ISBN:";
+            // 
+            // lblResultName
+            // 
+            this.lblResultName.AutoSize = true;
+            this.lblResultName.BackColor = System.Drawing.Color.AliceBlue;
+            this.lblResultName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultName.Location = new System.Drawing.Point(132, 253);
+            this.lblResultName.Name = "lblResultName";
+            this.lblResultName.Size = new System.Drawing.Size(0, 22);
+            this.lblResultName.TabIndex = 14;
+            // 
+            // lblResultAuthor
+            // 
+            this.lblResultAuthor.AutoSize = true;
+            this.lblResultAuthor.BackColor = System.Drawing.Color.AliceBlue;
+            this.lblResultAuthor.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultAuthor.Location = new System.Drawing.Point(146, 345);
+            this.lblResultAuthor.Name = "lblResultAuthor";
+            this.lblResultAuthor.Size = new System.Drawing.Size(0, 20);
+            this.lblResultAuthor.TabIndex = 15;
+            // 
+            // lblResultDescript
+            // 
+            this.lblResultDescript.AutoSize = true;
+            this.lblResultDescript.BackColor = System.Drawing.Color.AliceBlue;
+            this.lblResultDescript.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultDescript.Location = new System.Drawing.Point(202, 427);
+            this.lblResultDescript.Name = "lblResultDescript";
+            this.lblResultDescript.Size = new System.Drawing.Size(0, 20);
+            this.lblResultDescript.TabIndex = 16;
+            // 
+            // lblResultIsbn
+            // 
+            this.lblResultIsbn.AutoSize = true;
+            this.lblResultIsbn.BackColor = System.Drawing.Color.AliceBlue;
+            this.lblResultIsbn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultIsbn.Location = new System.Drawing.Point(132, 513);
+            this.lblResultIsbn.Name = "lblResultIsbn";
+            this.lblResultIsbn.Size = new System.Drawing.Size(0, 20);
+            this.lblResultIsbn.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(950, 567);
+            this.Controls.Add(this.lblResultIsbn);
+            this.Controls.Add(this.lblResultDescript);
+            this.Controls.Add(this.lblResultAuthor);
+            this.Controls.Add(this.lblResultName);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnKeyword);
+            this.Controls.Add(this.btnIsbn);
+            this.Controls.Add(this.btnAuthor);
+            this.Controls.Add(this.txtKeyword);
+            this.Controls.Add(this.txtIsbn);
+            this.Controls.Add(this.txtAuthor);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Modern No. 20", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtAuthor;
+        private System.Windows.Forms.TextBox txtIsbn;
+        private System.Windows.Forms.TextBox txtKeyword;
+        private System.Windows.Forms.Button btnAuthor;
+        private System.Windows.Forms.Button btnIsbn;
+        private System.Windows.Forms.Button btnKeyword;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lblResultName;
+        private System.Windows.Forms.Label lblResultAuthor;
+        private System.Windows.Forms.Label lblResultDescript;
+        private System.Windows.Forms.Label lblResultIsbn;
+    }
+}
+
---
+++ Hands On Test Assignments/CH06/PART-A/CH06/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CH06
+{
+    public partial class Form1 : Form
+    {
+        List<string> bookNames = new List<string> {"The Great Gatsby", "War and Peace", "Moby-Dick", "Hamlet", "Pride and Prejudice" };
+        List<string> keywords = new List<string> { "The story of eccentric millionaire Jay Gatsby \n and his pursuit of his lost love.", 
+            "A fictional story about the 1812 French invasion of Russia.", "The story of a sailor’s relentless hunt for a white whale.", 
+            "A Shakespearean tragedy about a young man coming home from college \n after the murder of his father.", "A comedic story of love and life \n in Old England." };
+        List<string> authors = new List<string> { "F. Scott Fitzgerald", "Leo Tolstoy", "Herman Melville", "William Shakespeare", "Jane Austen" };
+        List<string> isbnNumbers = new List<string> { "978-1847496140", "978-1400079988", "978-1503280786", "978-1973844402", "978-0141439518" };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //author search
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            ClearResults(); //clear old searches
+            int index = SearchByAuthor(txtAuthor.Text);
+            ShowBook(index); //call on method
+        }
+
+        //ISBN search
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ClearResults();
+            int index = SearchByIsbn(txtIsbn.Text);
+            ShowBook(index);
+        }
+
+        //keyword search
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ClearResults();
+            int index = SearchByKeyword(txtKeyword.Text);
+            ShowBook(index);
+
+        }
+
+        //Methods
+        private void ShowBook(int index)
+        {
+            if (index >= 0 && index < bookNames.Count)
+            {
+                lblResultName.Text = $" {bookNames[index]}";
+                lblResultAuthor.Text = $"{authors[index]}";
+                lblResultDescript.Text = $"{keywords[index]}";
+                lblResultIsbn.Text = $"{isbnNumbers[index]}";
+            }
+            else
+            {
+               
+                lblResultName.Text = $"Item not found.";
+                lblResultAuthor.Text = $"Item not found.";
+                lblResultDescript.Text = $"Item not found";
+                lblResultIsbn.Text = $"Item not found";
+            }
+        }
+        private int SearchByAuthor(string authorName)
+        {
+            for (int i = 0; i < authors.Count; i++)
+            { 
+                if (authors[i].ToLower().Contains(authorName.ToLower()))
+                    return i;
+            }
+            return -1;
+
+        }
+        private int SearchByIsbn(string isbnNumber)
+        {
+            for(int i = 0;i < isbnNumbers.Count;i++)
+            {
+                if (isbnNumbers[i] == isbnNumber)
+                    return i;
+            }
+            return -1;
+        }
+        private int SearchByKeyword(string keyword)
+        {
+            for (int i = 0; i < bookNames.Count; i++)
+            {
+                if (bookNames[i].ToLower().Contains(keyword) || keywords[i].ToLower().Contains(keyword)) 
+                    return i;
+            }
+            return -1;
+        }
+        private void ClearResults()
+        {
+            lblResultName.Text = string.Empty;
+            lblResultAuthor.Text = string.Empty;
+            lblResultDescript.Text= string.Empty;
+            lblResultIsbn.Text= string.Empty;
+            
+        }
+    }
+}
