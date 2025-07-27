eb7d3849bae5eb3b2bb1977bd316d05194ca8486
Cami-Carstens <camisuec@aol.com>
2025-07-27 15:19:32 -0500

    MauiInventoryItemCRUD-CH15

---
+++ MauiTutorials/CH15/InventoryItemCrud/MainPage.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections.ObjectModel;
+using InventoryItemCrud.Models;
+
+namespace InventoryItemCrud
+{
+    public partial class MainPage : ContentPage
+    {
+        public ObservableCollection<InventoryItem> InventoryItems { get; set; } = [];
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+            //seed data going into the collection
+            InventoryItems.Add(new InventoryItem(1000, "Laptop", 999.99m));
+            InventoryItems.Add(new InventoryItem(2000, "Mouse", 19.99m));
+
+            // Bind the collection to the ListView in the XAML
+            BindingContext = this;
+        }
+
+        private void btnAdd_Clicked(object sender, EventArgs e)
+        {
+            if(int.TryParse(ItemNoEntry.Text, out int itemNo)
+                && !string.IsNullOrWhiteSpace(DescriptionEntry.Text)
+                && decimal.TryParse(PriceEntry.Text, out decimal price)) 
+            {
+                //if data is valid, add it to the collection
+                InventoryItems.Add(new InventoryItem(itemNo, DescriptionEntry.Text, price));
+                ClearEntries();
+            }
+            else
+            {
+                DisplayAlert("Error", "Please fill in all fields.", "OK");
+            }
+
+
+        }
+
+        private void Updatebtn_Clicked(object sender, EventArgs e)
+        {
+
+            if (InventoryListView.SelectedItem is InventoryItem selectedItem)
+            {
+                if (int.TryParse(ItemNoEntry.Text, out int itemNo)
+                && !string.IsNullOrWhiteSpace(DescriptionEntry.Text)
+                && decimal.TryParse(PriceEntry.Text, out decimal price))
+                {
+                    //if data is valid, update the selected item, updating the properties values
+                    selectedItem.ItemNo = itemNo;
+                    selectedItem.Description = DescriptionEntry.Text;
+                    selectedItem.Price = price;
+
+                        //get the index of the selected item in the collection
+                    //InventoryItems[InventoryItems.IndexOf(selectedItem)] = selectedItem; // Update the collection
+                    //ClearEntries(); 
+
+
+                }
+                else
+                {
+
+                }
+            }
+        }
+
+        private void btnDelete_Clicked(object sender, EventArgs e)
+        {
+            //if data type is valid creates the variable selectedItem
+            if (InventoryListView.SelectedItem is InventoryItem selectedItem)
+            {
+                InventoryItems.Remove(selectedItem);
+                ClearEntries();
+            }
+
+        }
+
+        private void InventoryListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if(e.SelectedItem is InventoryItem selectedItem)
+            {
+                // Populate the entries with the selected item's data
+                ItemNoEntry.Text = selectedItem.ItemNo.ToString();
+                DescriptionEntry.Text = selectedItem.Description;
+                PriceEntry.Text = selectedItem.Price.ToString("F2");
+            }
+        }
+        private void ClearEntries()
+        {
+            ItemNoEntry.Text = string.Empty;
+            DescriptionEntry.Text = string.Empty;
+            PriceEntry.Text = string.Empty;
+
+        }
+    }
+}
---
+++ MauiTutorials/CH15/InventoryItemCrud/Models/InventoryItem.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryItemCrud.Models
+{
+
+              //primary constructor-newer syntax                  
+    public class InventoryItem(int itemNo, string description, decimal price) : INotifyPropertyChanged
+    {
+        private int _itemNo=itemNo;
+        private string _description=description;
+        private decimal _price=price;
+
+        public int ItemNo
+        {
+            get => _itemNo;
+            set
+            {
+               _itemNo = value;
+                //this is the instance of the inventory item that has changed, and event arguments with the name of the property that changed
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("ItemNo"));
+            }
+        }
+        public string Description
+        {
+            get => _description;
+            set
+            {
+                _description = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Description"));
+            }
+        }
+        public decimal Price
+        {
+            get => _price;
+            set
+            {
+                _price = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Price"));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("PriceDisplay"));
+            }
+        }
+        public string PriceDisplay => _price.ToString("C");
+        
+
+        //public InventoryItem(int itemNo, string description, decimal price)
+        //{
+        //    ItemNo = itemNo;
+        //    Description = description;
+        //    Price = price;
+        //}
+
+        //event has to be fired and then listened to in the XAML
+        //this is creating an event that will be triggered when a property changes
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
