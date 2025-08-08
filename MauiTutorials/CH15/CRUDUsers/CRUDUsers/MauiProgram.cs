1c28a5311a62f51f09ac7226b8a12b3b33ba349e
Cami-Carstens <camisuec@aol.com>
2025-08-07 19:42:01 -0500

    Maui-CH15

---
+++ MauiTutorials/CH15/CRUDUsers/CRUDUsers/MauiProgram.cs
@@ -0,0 +1,55 @@
+﻿using CRUDUsers.Data;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using CRUDUsers.Models;
+
+
+namespace CRUDUsers
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+            builder.Services.AddDbContext<DataContext>(
+               options =>
+               {
+                   var dbPath = Path.Combine(FileSystem.AppDataDirectory, "users.db");
+                    options.UseSqlite($"Data Source={dbPath}");
+               }
+                );
+            //dependency injection-allows us to inject dependencies into classes, rather than hard coding them(automatically) 
+
+#if DEBUG
+            builder.Logging.AddDebug();
+#endif
+            var app = builder.Build();
+
+            using (var scope = app.Services.CreateScope())
+            {
+                var context = scope.ServiceProvider.GetRequiredService<DataContext>();
+                context.Database.EnsureCreated(); //creates the database if it does not exist
+
+                if(!context.Users.Any()) //Any returns users if they exist
+                {
+                    context.Users.AddRange(
+                        new User { Name= "Alice" },
+                        new User { Name = "Bob" },
+                        new User { Name = "Charlie" }
+                        );
+                    context.SaveChanges(); //saves the changes to the database
+                }
+            }
+            return app;
+            
+        }
+    }
+}
---
+++ MauiTutorials/CH15/CRUDUsers/CRUDUsers/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUDUsers.Models
+{
+    //user is a Domain Model(ties to a data base)/ Model is another name for class
+    //view models is for front end, domain models are for the back end
+    public class User  
+    {
+        public int Id { get; set; }  //Primary key column
+        public required string Name { get; set; }  //User's name column 
+
+
+    }
+}
